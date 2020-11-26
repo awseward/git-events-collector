2e5706e3d9d42a78c085be921c66dd39b6294a67
Andrew Seward <awswrd@gmail.com>
2020-11-26 00:22:38 -0800

    Add some more boilerplate-type stuff

--- src/git_events_collector.nim
+++ src/git_events_collector.nim
@@ -1,7 +1,29 @@
+import argparse
+
+import ./statics
+
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
-
 import git_events_collectorpkg/submodule
 
-when isMainModule:
-  echo(getWelcomeMessage())
+const AppName = "git_events_collector"
+
+let p = newParser(AppName):
+  help "TODO"
+
+  flag("--version", help = "Print the version of " & AppName)
+  flag("--revision", help = "Print the Git SHA of " & AppName)
+  flag("--info", help = "Print version and revision")
+
+  run:
+    if opts.version:
+      echo pkgVersion
+    elif opts.revision:
+      echo pkgRevision
+    elif opts.info:
+      echo "version:  ", pkgVersion
+      echo "revision: ", pkgRevision
+    elif commandLineParams().len == 0:
+      echo p.help
+
+p.run
