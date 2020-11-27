236ef3f0a45d3e9c217846c1adfb7efe2be00b39
Andrew Seward <awswrd@gmail.com>
2020-11-26 21:49:16 -0800

    Add actual DateTime
    Wow, that's not great...

--- src/event_log.nim
+++ src/event_log.nim
@@ -1,34 +1,31 @@
 import sequtils
 import strutils
+import sugar
 import times
 
 type Event* = object
   version*: string
-  timestamp*: string # TODO: Maybe try making this an actual DateTime
+  timestamp*: DateTime
   hook*: string
   repo*: string
   `ref`*: string
 
+proc parseUtcISO(input: string): DateTime =
+  input.replace("T", " ").replace("Z", "").parse("yyyy-MM-dd HH:mm:ss", utc())
+
 proc fromLine*(line: string): Event =
   let parts = split(line, "\t")
-  echo parts
   Event(
     version: parts[0],
-    timestamp: parts[1],
+    timestamp: parseUtcISO(parts[1]),
     hook: parts[2],
     repo: parts[3],
     `ref`: parts[4]
   )
 
 proc fromFile*(path: string): seq[Event] =
-  readFile(path).splitLines.map fromline
+  readFile(path).splitLines.filter(line => line != "").map fromline
 
 # ---
 
 echo fromFile("/Users/andrew/.git-events.log")
-
-# let line = "0.1.0\t2020-11-26T08:23:09Z\t/Users/andrew/.git-hooks/pre-push\t/Users/andrew/projects/nim/git-events-collector\tmaster"
-
-# let event = fromLine line
-# echo ""
-# echo event
