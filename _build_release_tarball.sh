f1dc011f428bf52d11a4c9feb7582e05671da80c
Andrew Seward <awswrd@gmail.com>
2020-12-08 20:28:50 -0800

    Meh..

--- _build_release_tarball.sh
+++ _build_release_tarball.sh
@@ -8,8 +8,8 @@ readonly tag_name="$1"
 readonly binary_name='git_events_collector'
 readonly tarball_name="${binary_name}-${tag_name}.tar.gz"
 
-echo | xargs -t >&2 nimble build --accept --define:release
-echo | xargs -t >&2 tar -czf "${tarball_name}" \
+>&2 nimble build --accept --define:release
+>&2 tar -czf "${tarball_name}" \
                            "${binary_name}" \
                            rotate.sh \
                            to_sqlite.sh \
