5708a501d7a02c380512eabbaaa36d84c98a8235
Andrew Seward <awswrd@gmail.com>
2020-12-08 22:54:45 -0800

    Prefix "client" binaries w/ gec_
    Also, remove `.sh` extension.
    
    Since homebrew is just going to dump them into `/usr/local/bin`, can't
    have quite as generic names so we'll have to qualify them a little bit.
    
    They'll likey change again before long but this'll be fine for now.

--- mvp.sh
+++ mvp.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 readonly ingest_url="${DW_INGEST_URL:=localhost:8080}"'/sqlite?sj_path={}&store=git-events'
 
-./rotate.sh \
-  | xargs -t ./to_sqlite.sh \
-  | xargs -t ./push.sh \
+./gec_rotate \
+  | xargs -t ./gec_tsv-to-sqlite \
+  | xargs -t ./gec_push \
   | xargs -t -I{} curl "${ingest_url}"
--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-readonly filepath="$1"
-
-echo test-write | xargs -t uplink cp "${filepath}" 'sj://test/' --access | sed -E 's/^Created //'
