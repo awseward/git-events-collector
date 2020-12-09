3be3920eef23cdf25286169a78661cdede1a766d
Andrew Seward <awswrd@gmail.com>
2020-12-08 20:15:06 -0800

    Add some basic packaging

---
+++ _build_release_tarball.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly tag_name="$1"
+readonly binary_name='git_events_collector'
+readonly tarball_name="${binary_name}-${tag_name}.tar.gz"
+
+echo | xargs -t >&2 nimble build --accept --define:release
+echo | xargs -t >&2 tar -czf "${tarball_name}" \
+                           "${binary_name}" \
+                           rotate.sh \
+                           to_sqlite.sh \
+                           push.sh
+
+echo "${tarball_name}"
