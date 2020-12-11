3e267be5af8726d75d680ecb6ed5f48ffe7d7ef1
Andrew Seward <awswrd@gmail.com>
2020-12-10 19:36:43 -0800

    Pull nim stuff out of heroku_bootstrap.sh

--- dw/scripts/heroku_bootstrap.sh
+++ dw/scripts/heroku_bootstrap.sh
@@ -6,13 +6,10 @@ git remote show heroku \
   && >&2 echo -e "\nERROR: Found an existing git remote named 'heroku'. Aborting." \
   && exit 1
 
-heroku create --buildpack heroku/nodejs
-heroku buildpacks:add https://github.com/awseward/heroku-buildpack-nim
+heroku create
+
+heroku buildpacks:add https://github.com/heroku/heroku-buildpack-apt
+heroku buildpacks:add https://github.com/niteoweb/heroku-buildpack-shell
 heroku addons:create heroku-postgresql:hobby-dev
 
-heroku config:set IS_HEROKU=true
-heroku config:set NIM_BRANCH=devel
-heroku config:set NIM_REV=dc3919bb1af89799e391b4c4ecd0f1f60f7862ff
-heroku config:set NIMBLE_FLAGS='-d:release -d:ssl'
-
-git push heroku master
+git push heroku master:main
