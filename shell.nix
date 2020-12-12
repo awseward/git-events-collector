e791fba5a85dd2ac419ef5d9d55621f8f901fd6c
Andrew Seward <awswrd@gmail.com>
2020-12-11 22:32:42 -0800

    Remove _all_ DW stuff
    Looks like I missed a bunch of stuff the first time through...

--- shell.nix
+++ shell.nix
@@ -1,10 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
   with pkgs; mkShell {
     nativeBuildInputs = [
-      # Haven't gotten `freetds` to work via nix... For now, see the repo's
-      # Brewfile; e.g. `brew bundle install`
-      # freetds
-
-      pgloader
+      # ...
     ];
 }
